00db060059922fa5b722b07d52ec0fe60e3ea39f
vanduc006 <bagiadaohoa@gmail.com>
2025-10-25 14:54:16 +0700

    add fucntion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,30 +7,74 @@ import './App.css'
 
 function App() {
   const [currentTodo, setCurrent] = useState([]);
+  const [currentId, setCurrentId] = useState(3);
+  const [newItem, setNewItem] = useState("");
+
+  // setCurrent(currentId+1)
+  /// ...prev
 
   const data = [
     {
+      "id" : "1",
       "item": "Mua sua"
     },
     {
-      "iteam": "Hoc React"
+      "id" : "2",
+      "item": "Hoc React"
     },
     {
+      "id" : "3",
       "item": "Chay bo"
     }
   ]
+
+  const add = async(value) => {
+    // console.log(newItem)
+    setCurrent([...currentTodo, {
+      item : value,
+      id : currentId,
+    } ])
+  }
+
+  const remove = () => {
+
+  }
+
+  useEffect(() => {
+    setCurrent(data)
+  },[])
+
   return (
     <>
-      <div class="container">
-        <h1>Danh Sách Công Việc</h1>
-        <div class="add-form">
-          <input type="text" placeholder="Nhập công việc mới..."/>
-          <button>Thêm</button>
+      <div className="container">
+        <h1>Danh sách Công Việc</h1>
+        <div className="add-form">
+          <input value={newItem} 
+
+          onChange={async(e) =>  {
+            setCurrentId(currentId+1)
+            setNewItem(e.target.value) 
+          }
+            // console.log(e.target.value)
+            
+          } 
+          type="text" placeholder="Nhập công việc mới..."/>
+
+          <button onClick={() => {
+            add(newItem)
+          }}>Thêm</button>
         </div>
         <ul>
-          <li>Mua sữa <button>Xóa</button></li>
+          {currentTodo.map((todo,idx) => {
+            return (
+              <div key={idx}>
+                <li>{todo.item}<button>Xóa</button></li>
+              </div>
+            )
+          })}
+          {/* <li>Mua sữa <button>Xóa</button></li>
           <li>Học React <button>Xóa</button></li>
-          <li>Chạy bộ <button>Xóa</button></li>
+          <li>Chạy bộ <button>Xóa</button></li> */}
         </ul>
       </div>
     </>

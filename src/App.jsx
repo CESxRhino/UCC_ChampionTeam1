0829ce9f98dce0bfc6ad57f2d61770b0cf6047c5
vanduc006 <bagiadaohoa@gmail.com>
2025-10-25 15:14:53 +0700

    fix id in add

--- src/App.jsx
+++ src/App.jsx
@@ -9,66 +9,84 @@ function App() {
   const [currentTodo, setCurrent] = useState([]);
   const [currentId, setCurrentId] = useState(3);
   const [newItem, setNewItem] = useState("");
+  const [selectedItem, setSelectedItem] = useState();
 
   // setCurrent(currentId+1)
   /// ...prev
 
   const data = [
     {
-      "id" : "1",
+      "id": "1",
       "item": "Mua sua"
     },
     {
-      "id" : "2",
+      "id": "2",
       "item": "Hoc React"
     },
     {
-      "id" : "3",
+      "id": "3",
       "item": "Chay bo"
     }
   ]
 
-  const add = async(value) => {
+  const add = async (value) => {
     // console.log(newItem)
+    setCurrentId(currentId+1)
+    console.log(currentId)
     setCurrent([...currentTodo, {
-      item : value,
-      id : currentId,
-    } ])
+      item: value,
+      id: currentId+1,
+    }])
   }
 
-  const remove = () => {
+  const remove = (id) => {
+    // setCurrent((prev) => {
+    //   prev.filter((todo) => {
+    //     todo.id !== id
+    //   })
+    // })
 
+    setCurrent(currentTodo.filter(todo => {
+      todo.id !== id
+    }))
   }
 
   useEffect(() => {
     setCurrent(data)
-  },[])
+  }, [])
 
   return (
     <>
       <div className="container">
         <h1>Danh sách Công Việc</h1>
         <div className="add-form">
-          <input value={newItem} 
+          <input value={newItem}
 
-          onChange={async(e) =>  {
-            setCurrentId(currentId+1)
-            setNewItem(e.target.value) 
-          }
-            // console.log(e.target.value)
-            
-          } 
-          type="text" placeholder="Nhập công việc mới..."/>
+            onChange={async (e) => {
+              setNewItem(e.target.value)
+            }
+              // console.log(e.target.value)
+
+            }
+            type="text" placeholder="Nhập công việc mới..." />
 
           <button onClick={() => {
             add(newItem)
           }}>Thêm</button>
         </div>
         <ul>
-          {currentTodo.map((todo,idx) => {
+          {currentTodo.map((todo, idx) => {
             return (
               <div key={idx}>
-                <li>{todo.item}<button>Xóa</button></li>
+                <li>{todo.id} | {todo.item}
+                <button 
+                onClick={() => {
+                  setSelectedItem(todo.id)
+                  remove(todo.id)
+                }
+                }
+
+                >Xóa</button></li>
               </div>
             )
           })}

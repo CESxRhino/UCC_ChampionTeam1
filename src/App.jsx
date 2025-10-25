845e74fd555728d6086503645504e1963dc2f4b9
vanduc006 <bagiadaohoa@gmail.com>
2025-10-25 14:24:44 +0700

    first UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+function App() {
+  const [currentTodo, setCurrent] = useState([]);
+
+  const data = [
+    {
+      "item": "Mua sua"
+    },
+    {
+      "iteam": "Hoc React"
+    },
+    {
+      "item": "Chay bo"
+    }
+  ]
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="container">
+        <h1>Danh Sách Công Việc</h1>
+        <div class="add-form">
+          <input type="text" placeholder="Nhập công việc mới..."/>
+          <button>Thêm</button>
+        </div>
+        <ul>
+          <li>Mua sữa <button>Xóa</button></li>
+          <li>Học React <button>Xóa</button></li>
+          <li>Chạy bộ <button>Xóa</button></li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
